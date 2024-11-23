1da4450c51d74c2af76e91086d9c4a82bea257bb
RaiSamiksha <samiksharai130@gmail.com>
2024-11-24 01:26:26 +0530

    change in docs folder. added new docs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { ToDoListComponent } from '../to-do-list/to-do-list.component';
 
 export const routes: Routes = [
+    { path: '', component: ToDoListComponent},
     { path: 'to-do', component: ToDoListComponent},
 ];
